0e4507c8aa1bc42e68dadcf415af6cad856cb0cb
lazaroAM <44584192+lazaroAM@users.noreply.github.com>
2020-04-02 09:41:01 -0300

    formik e yup

--- src/pages/Todos/index.js
+++ src/pages/Todos/index.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import {Creators} from '../../store/ducks/todos';
 import {useSelector, useDispatch} from 'react-redux';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 import {
   Container,
   Input,
@@ -17,64 +19,75 @@ import {
   EditText,
 } from './styles';
 
+const schema = Yup.object().shape({
+  txt: Yup.string().required('Campo vazio').min(2, 'No minimo 2 letras'),
+});
+
 function TodoList() {
   const todos = useSelector((state) => state.todos);
   const dispatch = useDispatch();
-  const [todo, setTodo] = useState({id: null, txt: ''});
 
   useEffect(() => {
     dispatch(Creators.fetchTodos());
   }, [dispatch]);
 
-  function addEdit() {
-    if (!todo.id) {
-      dispatch(Creators.addTodo(todo.txt));
-    } else {
-      dispatch(Creators.editTodo(todo.id, todo.txt));
-    }
-    setTodo({id: null, txt: ''});
-  }
-
   function remove(id) {
     dispatch(Creators.removeTodo(id));
   }
 
-  function edit(item) {
-    setTodo({id: item.id, txt: item.txt});
+  function onSubmit(values, {resetForm}) {
+    if (!values.id) {
+      dispatch(Creators.addTodo(values.txt));
+    } else {
+      dispatch(Creators.editTodo(values.id, values.txt));
+    }
+    resetForm();
   }
 
   return (
-    <Container>
-      <Input
-        placeholder={'Adicione um texto'}
-        value={todo.txt}
-        onChangeText={(text) => setTodo({...todo, txt: text})}
-      />
+    <Formik
+      initialValues={{id: null, txt: ''}}
+      onSubmit={onSubmit}
+      validationSchema={schema}>
+      {({handleSubmit, handleChange, values, errors, setValues}) => {
+        console.log(values);
+        return (
+          <Container>
+            <Input
+              placeholder={'Adicione um texto'}
+              value={values.txt}
+              onChangeText={handleChange('txt')}
+            />
+            {errors.txt ? <AddText>{errors.txt}</AddText> : null}
+            <AddButton onPress={handleSubmit}>
+              <AddText>{!values.id ? 'Adicionar' : 'Editar'}</AddText>
+            </AddButton>
 
-      <AddButton onPress={addEdit}>
-        <AddText>{!todo.id ? 'Adicionar' : 'Editar'}</AddText>
-      </AddButton>
+            <Div>
+              <Title>TODOS</Title>
+            </Div>
 
-      <Div>
-        <Title>TODOS</Title>
-      </Div>
+            {todos.data.map((item) => (
+              <TodoDiv key={item.id}>
+                <TodoText>{item.txt}</TodoText>
 
-      {todos.data.map((item) => (
-        <TodoDiv key={item.id}>
-          <TodoText>{item.txt}</TodoText>
+                <ButtonDiv>
+                  <EditButton onPress={() => setValues(item)}>
+                    <EditText>Editar</EditText>
+                  </EditButton>
 
-          <ButtonDiv>
-            <EditButton onPress={() => edit(item)}>
-              <EditText>Editar</EditText>
-            </EditButton>
-
-            <RemoveButton title={'remover'} onPress={() => remove(item.id)}>
-              <RemoveText>Remover</RemoveText>
-            </RemoveButton>
-          </ButtonDiv>
-        </TodoDiv>
-      ))}
-    </Container>
+                  <RemoveButton
+                    title={'remover'}
+                    onPress={() => remove(item.id)}>
+                    <RemoveText>Remover</RemoveText>
+                  </RemoveButton>
+                </ButtonDiv>
+              </TodoDiv>
+            ))}
+          </Container>
+        );
+      }}
+    </Formik>
   );
 }
 
