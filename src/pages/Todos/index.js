2a6d0fce1054bdc200149357204f0ed38fdcf204
lazaroAM <44584192+lazaroAM@users.noreply.github.com>
2020-04-02 09:43:36 -0300

    formik e yup

--- src/pages/Todos/index.js
+++ src/pages/Todos/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import {Creators} from '../../store/ducks/todos';
 import {useSelector, useDispatch} from 'react-redux';
 import {Formik} from 'formik';
